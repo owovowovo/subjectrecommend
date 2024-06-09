c72ff7dd6c0b5d118320e439c1247c2ec87b335e
owovowovo <162708460+owovowovo@users.noreply.github.com>
2024-06-09 23:18:16 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from departments import department
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    dream = request.form.get('dream')
+    recommended_courses = department.get(dream)
+
+    return render_template('result.html', dream=dream, courses=recommended_courses)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
